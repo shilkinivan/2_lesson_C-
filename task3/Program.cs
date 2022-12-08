176bf49f044b964ff7ed81dc975f27406f9d4c96
Ivan <shilkin-ivan@mail.ru>
2022-12-09 00:18:54 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 23 == 0)
+{
+    if (n % 7 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Нет");
+}
