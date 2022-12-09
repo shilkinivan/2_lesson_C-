e8c0f88a80ca25ce1246cab854e8ae0c160c3dfb
Ivan <shilkin-ivan@mail.ru>
2022-12-09 19:30:23 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string i =Convert.ToString(n);
+if (n>99)
+{
+Console.WriteLine(i[2]); 
+}
+else
+{
+Console.WriteLine("Третьей цифры нет");
+}
